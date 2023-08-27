69d514fadfbd8fa75bda88da055800ae6bcb5b00
Magesh Dhasayyan <mgsh@users.noreply.github.com>
2023-08-27 23:13:02 +0200

    wip

--- lua/pril/plugins/coding.lua
+++ lua/pril/plugins/coding.lua
@@ -5,6 +5,7 @@ return {
         -- follow latest release.
         version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     },
+
     -- comments
     { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
     {
@@ -20,15 +21,51 @@ return {
     },
 
     -- auto completion
-    --    {
-    --        "hrsh7th/nvim-cmp",
-    --        version = false, -- last release is way too old
-    --        event = "InsertEnter",
-    --        dependencies = {
-    --            "hrsh7th/cmp-nvim-lsp",
-    --            "hrsh7th/cmp-buffer",
-    --            "hrsh7th/cmp-path",
-    --            "saadparwaiz1/cmp_luasnip",
-    --        },
-    --    }
+    {
+        "hrsh7th/nvim-cmp",
+        version = false, -- last release is way too old
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        opts = function()
+            local cmp = require("cmp")
+            return {
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<S-CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+            }
+        end
+    },
+
+    -- auto pairs
+    {
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        opts = {},
+    },
 }
