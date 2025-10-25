2ce182ae833b63546887bc78dc8f6667adb97565
Magesh Dhasayyan <215458744+magesh-slacker@users.noreply.github.com>
2025-10-25 12:58:43 +0530

    Update leap config

--- lua/pril/plugins/leap.lua
+++ lua/pril/plugins/leap.lua
@@ -1,19 +1,47 @@
 return {
-  -- TODO: try out leap-spooky
   {
     "ggandor/leap.nvim",
     version = false,
     event = "BufReadPost",
-    keys = {
-      { "<leader>/", "<Plug>(leap-forward-to)",    mode = "n",          desc = "Leap forward to" },
-      { "<leader>?", "<Plug>(leap-backward-to)",   mode = "n",          desc = "Leap backward to" },
-      { "<leader>/", "<Plug>(leap-forward-till)",  mode = { "x", "o" }, desc = "Leap forward till" },
-      { "<leader>?", "<Plug>(leap-backward-till)", mode = { "x", "o" }, desc = "Leap backward till" },
-      { "gs",        "<Plug>(leap-from-window)",   mode = "n",          desc = "Leap from window" },
-    },
+
     config = function()
-      vim.api.nvim_set_hl(0, "LeapLabel", { link = "IncSearch" })
-      vim.api.nvim_set_hl(0, "LeapLabelPrimary", { link = "IncSearch" })
+      local leap = require("leap")
+      local modes = { "n", "x", "o" }
+
+      ------------------------------------------------------------------------
+      -- 1. Default 2-char searches ("s" and "S")
+      ------------------------------------------------------------------------
+      -- Forward search (buffer only)
+      vim.keymap.set(modes, "s", "<Plug>(leap-forward)", { desc = "Leap forward (2-char)" })
+      -- Backward search (buffer only)
+      vim.keymap.set(modes, "S", "<Plug>(leap-backward)", { desc = "Leap backward (2-char)" })
+
+      ------------------------------------------------------------------------
+      -- 2. Simplified custom f/t motions (1-char search)
+      ------------------------------------------------------------------------
+      do
+        -- Helper function to generate the one-character motion function
+        local function ft_motion(backward, offset)
+          return function()
+            local args = {
+              backward = backward,
+              offset = offset,
+              inputlen = 1, -- Restrict to one character
+              inclusive = true,
+              opts = {
+                labels = "", -- Force autojump
+              },
+            }
+            leap.leap(args)
+          end
+        end
+
+        -- Set the mappings for f, F, t, T (in all relevant modes)
+        vim.keymap.set({ "n", "x", "o" }, "f", ft_motion(false, nil), { desc = "Leap forward 'f' (1-char)" })
+        vim.keymap.set({ "n", "x", "o" }, "F", ft_motion(true, nil), { desc = "Leap backward 'F' (1-char)" })
+        vim.keymap.set({ "n", "x", "o" }, "t", ft_motion(false, -1), { desc = "Leap forward 't' (1-char till)" })
+        vim.keymap.set({ "n", "x", "o" }, "T", ft_motion(true, 1), { desc = "Leap backward 'T' (1-char till)" })
+      end
     end,
   }
 }
